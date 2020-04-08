1a956983609b98d06a9fd0bf081cd7dffb3b5945
Bahovchuk Ivan <ivan1337bahovchuk@mail.ru>
2020-04-08 21:39:40 +0300

    Lesson3Task1

---
+++ src/by/academy/lesson3/Task1Cycles/Task1Cycles.java
@@ -0,0 +1,14 @@
+package by.academy.lesson3.Task1Cycles;
+
+/**
+ * Created by user on 08.04.2020.
+ */
+public class Task1Cycles {
+    public static void main(String[] args) {
+        int x = 10;
+        while (x <= 20) {
+            System.out.println(x*x);
+            x++;
+        }
+    }
+}
